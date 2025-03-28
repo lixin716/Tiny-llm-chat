efd0dacedd8d9895a0aa1262829e20cfc519d2fd
axin <96181076+lixin716@users.noreply.github.com>
2025-03-28 15:48:24 +0800

    Add files via upload

---
+++ model_train/data_process.py
@@ -0,0 +1,244 @@
+import json
+import glob
+import numpy as np
+from tqdm import tqdm
+from chatglm_tokenizer.tokenization_chatglm import ChatGLMTokenizer
+import pandas as pd
+#from zhconv import convert
+def process_wiki_clean():
+    with open('./data/wikipedia-cn-20230720-filtered.json','r',encoding='utf-8') as f:
+        data=json.load(f)
+    doc_ids=[]
+    for line in tqdm(data):
+        text=line['completion']
+        text_id=tokenizer.encode(text,add_special_tokens=False)
+        text_id.append(tokenizer.special_tokens['<eos>'])
+        if len(text_id)>5:
+            doc_ids+=text_id
+    arr = np.array(doc_ids,dtype=np.uint16)
+    with open('./data/wiki.bin','wb') as f:
+        f.write(arr.tobytes())
+
+def process_medical(data_path,name):
+    f=open(data_path,'r',encoding='utf-8')
+    doc_ids=[]
+    while True:
+        line=f.readline()
+        if not line:
+            break
+        line=json.loads(line)
+        text=line['text']
+        text_id=tokenizer.encode(text,add_special_tokens=False)
+        text_id.append(tokenizer.special_tokens['<eos>'])
+        if len(text_id)>5:
+            doc_ids+=text_id
+    arr = np.array(doc_ids,dtype=np.uint16)
+    with open('./data/medical_{}.bin'.format(name),'wb') as f:
+        f.write(arr.tobytes()) 
+
+def sft_to_pretrain():
+    doc_ids=[]
+
+    '''
+    df=pd.read_csv('./data/medical_qa_144w.csv')
+    for _,q,a in tqdm(df.itertuples()):
+        q_id = tokenizer.encode(q,add_special_tokens=False)
+        a_id = tokenizer.encode(a,add_special_tokens=False)
+        #
+        print(q)
+        print(a)
+        print('-----')
+        text_id=q_id+a_id+[tokenizer.special_tokens['<eos>']]
+        if len(text_id)>5:
+            doc_ids+=text_id
+    '''
+
+    with open('./data/train_en_1.json','r',encoding='utf-8') as f:
+        for row in f:
+            line=json.loads(row)
+            q=line['input']
+            a=line['output']
+            q_id=tokenizer.encode(q,add_special_tokens=False)
+            a_id=tokenizer.encode(a,add_special_tokens=False)
+            text_id=q_id+a_id+[tokenizer.special_tokens['<eos>']]
+            if len(text_id)>5:
+                doc_ids+=text_id
+    with open('./data/test_en_1.json','r',encoding='utf-8') as f:
+        for row in f:
+            line=json.loads(row)
+            q=line['input']
+            a=line['output']
+            q_id=tokenizer.encode(q,add_special_tokens=False)
+            a_id=tokenizer.encode(a,add_special_tokens=False)
+            text_id=q_id+a_id+[tokenizer.special_tokens['<eos>']]
+            if len(text_id)>5:
+                doc_ids+=text_id
+    with open('./data/valid_en_1.json','r',encoding='utf-8') as f:
+        for row in f:
+            line=json.loads(row)
+            q=line['input']
+            a=line['output']
+            q_id=tokenizer.encode(q,add_special_tokens=False)
+            a_id=tokenizer.encode(a,add_special_tokens=False)
+            text_id=q_id+a_id+[tokenizer.special_tokens['<eos>']]
+            if len(text_id)>5:
+                doc_ids+=text_id
+
+    with open('./data/train_zh_0.json','r',encoding='utf-8') as f:
+        for row in f:
+            line=json.loads(row)
+            q=line['instruction']+line['input']
+            a=line['output']
+            q_id=tokenizer.encode(q,add_special_tokens=False)
+            a_id=tokenizer.encode(a,add_special_tokens=False)
+            text_id=q_id+a_id+[tokenizer.special_tokens['<eos>']]
+            if len(text_id)>5:
+                doc_ids+=text_id
+    with open('./data/test_zh_0.json','r',encoding='utf-8') as f:
+        for row in f:
+            line=json.loads(row)
+            q=line['instruction']+line['input']
+            a=line['output']
+            q_id=tokenizer.encode(q,add_special_tokens=False)
+            a_id=tokenizer.encode(a,add_special_tokens=False)
+            text_id=q_id+a_id+[tokenizer.special_tokens['<eos>']]
+            if len(text_id)>5:
+                doc_ids+=text_id
+    with open('./data/valid_zh_0.json','r',encoding='utf-8') as f:
+        for row in f:
+            line=json.loads(row)
+            q=line['instruction']+line['input']
+            a=line['output']
+            q_id=tokenizer.encode(q,add_special_tokens=False)
+            a_id=tokenizer.encode(a,add_special_tokens=False)
+            text_id=q_id+a_id+[tokenizer.special_tokens['<eos>']]
+            if len(text_id)>5:
+                doc_ids+=text_id
+
+    arr = np.array(doc_ids,dtype=np.uint16)
+    print(arr.shape)
+    with open('./data/medical_qa.bin','wb') as f:
+        f.write(arr.tobytes())
+
+def sft_process():
+    with open('./data/alpaca_gpt4_data_zh.json','r',encoding='utf-8') as f:
+        data=json.load(f)
+    #
+    q_lst=[]
+    a_lst=[]
+    for per in data:
+        q=per['instruction']
+        i=per['input']
+        a=per['output']
+        q=q+i
+        if len(q)<10 or len(a)<5:
+            continue
+        if len(q)>256 or len(a)>256:
+            continue
+        q_lst.append(q)
+        a_lst.append(a)
+    #
+    # with open('../track1/train_valid.json','r') as f:
+    #     data=json.load(f)
+    # #
+    # for l in data:
+    #     q_lst.append(l['question'])
+    #     a_lst.append(l['answer'])
+    #
+    f = open('./data/Belle_open_source_1M.json','r',encoding='utf-8')
+    
+    #s
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        per=json.loads(line)
+        q=per['instruction']
+        i=per['input']
+        a=per['output']
+        q=q+i
+        if len(q)<10 or len(a)<5:
+            continue
+        if len(q)>256 or len(a)>256:
+            continue
+        q_lst.append(q)
+        a_lst.append(a)
+    df=pd.DataFrame(columns=['prompt','answer'])
+    df['prompt']=q_lst
+    df['answer']=a_lst
+    df.to_csv('data/sft_data.csv',index=False)
+    print(df)
+
+def process_baidu():
+    BATCH_SIZE = 1000000
+
+    cnt=0
+    batch_cnt=0
+    token=0
+    doc_ids=[]
+
+    f1=open('./data/563w_baidubaike.json','r',encoding='utf-8')
+    
+    while True:
+        line = f1.readline()
+        if not line:
+            break
+        line=json.loads(line)
+        text=''
+        try:
+            text+=line['title']+'：'+line['summary']
+        except:
+            pass
+        for per in line['sections']:
+            text+=per['title']+'：'+per['content']+'。'
+        text_id=tokenizer.encode(text,add_special_tokens=False)
+        text_id.append(tokenizer.special_tokens['<eos>'])
+        if len(text_id)>5:
+            doc_ids+=text_id
+        cnt+=1
+        if cnt%BATCH_SIZE==0:
+            batch_cnt+=1
+            arr = np.array(doc_ids,dtype=np.uint16)
+            doc_ids=[]
+            print('cnt:',cnt,'arr_shape:',arr.shape)
+            with open('./data/baidubaike_563w_{}.bin'.format(batch_cnt),'wb') as f2:
+                f2.write(arr.tobytes())
+            del arr
+
+    if not doc_ids:
+        batch_cnt+=1
+        arr = np.array(doc_ids,dtype=np.uint16)
+        print('cnt:',cnt,'arr_shape:',arr.shape)
+        with open('./data/baidubaike_563w_{}.bin'.format(batch_cnt),'wb') as f:
+            f.write(arr.tobytes())
+    
+
+    
+if __name__=="__main__":
+    tokenizer=ChatGLMTokenizer(vocab_file='./chatglm_tokenizer/tokenizer.model')
+    # process_wiki_clean()
+    # process_medical('./data/medical_book_zh.json','book')
+    # process_medical('./data/train_encyclopedia.json','encyclopedia')
+    # sft_to_pretrain()
+    # sft_process()
+    process_baidu()
+    data_path_list=[
+        './data/baidubaike_563w_1.bin',
+        './data/baidubaike_563w_2.bin',
+        './data/baidubaike_563w_3.bin',
+        './data/baidubaike_563w_4.bin',
+        './data/baidubaike_563w_5.bin',
+        './data/medical_book.bin',
+        './data/medical_encyclopedia.bin',
+        './data/wiki.bin',
+        './data/medical_qa.bin'
+    ]
+    data_lst=[]
+    for data_path in data_path_list:
+        with open(data_path,'rb') as f:
+            data=np.fromfile(f,dtype=np.uint16)
+            data_lst.append(data)
+    arr = np.concatenate(data_lst)
+    print(arr.shape)
+    with open('./data/pretrain_data.bin','wb') as f:
+        f.write(arr.tobytes())
---
+++ model_train/sft_loss.py
@@ -0,0 +1,38 @@
+import re
+import matplotlib.pyplot as plt
+
+# 读取log文件
+log_file_path = './out/sft_log.log'
+with open(log_file_path, 'r') as file:
+    log_data = file.readlines()
+
+# 提取批次和损失值
+batch_pattern = re.compile(r'Epoch:\[(\d+)/(\d+)\]\((\d+)/(\d+)\) loss:(\d+\.\d+)')
+batches = []
+losses = []
+batch_count = 0
+batch_interval = 1000  # 设置每1000个batch记录一次损失值
+
+for line in log_data:
+    match = batch_pattern.search(line)
+    if match:
+        epoch_num = int(match.group(1))
+        current_batch = int(match.group(3))
+        total_batches = int(match.group(4))
+        loss = float(match.group(5))
+        batch_count += 100
+        if batch_count % batch_interval == 0:
+            batches.append(current_batch + (epoch_num - 1) * total_batches + (batch_count // batch_interval - 1) * total_batches)
+            losses.append(loss)
+
+# 绘制图表
+plt.plot(batches, losses, label='Loss')
+plt.title(f'SFT Loss Over Batches (Every {batch_interval} Batches)')
+plt.xlabel('Batch')
+plt.ylabel('Loss')
+
+# 设置纵坐标范围
+plt.ylim(0, 8)
+
+plt.legend()
+plt.show()
